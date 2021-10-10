9703216b44e7c05f89d84c73668a9d030fe83bb9
Baltazar Cortina <baltazarcortina@gmail.com>
2021-10-10 12:36:40 -0300

    Exercise 01 - Variables and operators

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,35 @@
+// Variables y Operadores
+
+// 1. Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+console.log('Exercise 1:');
+
+var num1 = 15;
+var num2 = 6;
+
+var num3 = num1 + num2;
+
+console.log(num1 + ' + ' + num2 + ' = ' + num3);
+
+// 2. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+console.log('\nExercise 2:');
+
+var string1 = 'First string';
+var string2 = 'Second string';
+
+var string3 = string1 + string2;
+
+console.log(string1 + ' + ' + string2 + ' = ' + string3);
+
+
+// 3. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+console.log('\nExercise 3:');
+
+var string4 = 'One string';
+var string5 = 'Another string';
+
+var totalLength = string1.length + string2.length;
+
+console.log('The total length of "' + string4 + '" and "' + string5 + '" is ' + totalLength);
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Become a Software Developer
+
+## Problemática semanal 04
+
+- JavaScript Exercises
+
+## Author
+
+Baltazar Cortina
+
+---
+
+GitHub repository: https://github.com/BaltazarCortina/cortina-b-basd-2021-class-4
